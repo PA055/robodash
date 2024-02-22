4e712058cd04a4e7383d93e4ab7e38b14b2bfc80
Thurston Yates <thurstonalexyates@gmail.com>
2024-02-21 21:27:29 -0600

    Create basic GUI

--- src/robodash/views/graph.cpp
+++ src/robodash/views/graph.cpp
@@ -5,8 +5,21 @@
 // ============================== Constructor ============================== //
 
 rd::Graph::Graph(std::string name, int update_rate) {
+	// Set properties
 	this->rate = update_rate;
 	this->name = name;
+
+	// Create GUI
+	// TODO: Make gui
+	rd_view_t *view = rd_view_create(name.c_str());
+	lv_obj_t *cont = rd_view_obj(view);
+
+	chart = lv_chart_create(cont);
+	lv_obj_align(chart, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_set_size(chart, 416, 208);
+
+	// Run background task
+	// FIXME: Correct way to do this?
 	pros::Task task(&rd::Graph::update_task, "Graph updater");
 }
 
