0ceb7db89757f83d6dce3b7d37cf0576312599cb
Thurston Yates <thurstonalexyates@gmail.com>
2024-04-05 21:47:10 -0400

    Make cpp views wrap existing views

--- include/robodash/view.hpp
+++ include/robodash/view.hpp
@@ -4,23 +4,11 @@
 namespace rd {
 
 /**
- * @brief Flags that can alter the behavior of a View
- */
-enum class ViewFlag {
-	NoAnimation,
-	Hidden,
-};
-
-/**
  * @brief View class
  */
 class View {
   private:
-	std::vector<ViewFlag> flags;
-	std::string name;
-
-	lv_obj_t *list_btn;
-	lv_obj_t *obj;
+	rd_view_t *c_view;
 
   public:
 	/**
@@ -29,14 +17,6 @@ class View {
 	 * @param name Name of the View
 	 */
 	View(std::string name);
-
-	/**
-	 * @brief Create a View
-	 *
-	 * @param name Name of the View
-	 * @param flags Flags passed to the View
-	 */
-	View(std::string name, std::vector<ViewFlag> flags);
 
 	/**
 	 * @brief Set this View to the currently active View
@@ -48,14 +28,28 @@ class View {
 	 *
 	 * @param flag Flag to add
 	 */
-	void add_flag(ViewFlag flag);
+	void add_flag(uint32_t flag);
 
 	/**
 	 * @brief Remove a flag from the View
 	 *
 	 * @param flag Flag to clear
 	 */
-	void clear_flag(ViewFlag flag);
+	void remove_flag(uint32_t flag);
+
+	/**
+	 * @brief Get the C view object
+	 *
+	 * @return rd_view_t*
+	 */
+	rd_view_t *get_c_view();
+
+	/**
+	 * @brief Cast to an rd_view_t
+	 *
+	 * @return rd_view_t *
+	 */
+	operator rd_view_t *() const;
 
 	/**
 	 * @brief Get the View's lvgl object
