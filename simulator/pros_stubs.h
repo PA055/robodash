e976960d575c74fb68f5f46b343063f8ebffe30b
Thurston Yates <thurstonalexyates@gmail.com>
2024-05-17 14:11:30 -0400

    Fix stubs

--- simulator/pros_stubs.h
+++ simulator/pros_stubs.h
@@ -2,11 +2,14 @@
 
 #ifdef SIMULATOR
 #ifdef __cplusplus
+#include <chrono>
+#include <thread>
+
 namespace pros {
 namespace usd {
 inline bool is_installed() { return false; }
-inline void delay() { return; }
 } // namespace usd
+inline void delay(int msec) { std::this_thread::sleep_for(std::chrono::milliseconds(msec)); }
 } // namespace pros
 #endif
 #endif
